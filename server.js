603f97a30afc12f0671b40a79fc4a7803d8dc383
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-12-03 18:04:44 +0200

    feat: add get requests

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ import express from 'express';
 
 const app = new express();
 
+app.get('/', (req, res) => {
+    res.send("Hello from get request")
+})
+
+app.get('/api/', (req, res) => {
+    res.send("Hello from get api request");
+})
+
+app.get('/api/students/', (req, res) => {
+    res.send("Students list");
+})
+
+app.get('/api/students/:id', (req, res) => {
+    res.send('student number: ' + req.params.id );
+})
+
 app.listen(4000, () => {
     console.log('Server running at port: ' + 4000)
 });
