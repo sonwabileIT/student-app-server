e25973f63a3bed0a2bcc669fade54a0d3c374a82
Sonwabile <123471279+sonwabileIT@users.noreply.github.com>
2025-10-19 17:36:51 +0200

    feat: add studentController and apply changes to studentRoute

--- src/routes/studentRoute.ts
+++ src/routes/studentRoute.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { getAllStudentsController, postNewStudentController, getStudentByIdController, deleteStudentByIdController, updateStudentByIdController } from '../controllers/studentController';
 //import bodyParser from 'body-parser',
 
 const app = express();
@@ -9,58 +10,11 @@ const router = express.Router();
 
 //get all students
 //works
-router.get('/api/students/', async (req: Request, res: Response) => {
-  try {
-    const students = await fetch('http://localhost:3000/students');
-    const response = await students.json();
-    if (response.length !== 0) {
-      console.log(response);
-      res.json(response);
-    } else {
-      res.send('There are no students');
-    }
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
-});
+router.get('/api/students/', getAllStudentsController);
 
 //post a student
 //works
-router.post('/api/students/', (req: Request, res: Response) => {
-  const id: string = req.body.id;
-  const studentFirstName: string = req.body.studentFirstName;
-  const studentLastName: string = req.body.studentLastName;
-  const studentEmail: string = req.body.studentEmail;
-
-
-  const newStudent = {
-    id: id,
-    studentFirstName: studentFirstName,
-    studentLastName: studentLastName,
-    studentEmail: studentEmail
-  }
-
-  try {
-
-    const postStudent = fetch('http://localhost:3000/students', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "id": newStudent.id,
-        "studentFirstName": newStudent.studentFirstName,
-        "studentLastName": newStudent.studentLastName,
-        "studentEmail": newStudent.studentEmail
-      }),
-    });
-    res.status(201).end();
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
-});
+router.post('/api/students/', postNewStudentController);
 
 //delete all students
 //not working
@@ -88,69 +42,14 @@ router.post('/api/students/', (req: Request, res: Response) => {
 
 //get a student
 //works
-router.get(`/api/students/:id`, async (req: Request, res: Response) => {
-  const studentId = req.params.id;
-  if (studentId !== null) {
-
-    try {
-      const findStudent = await fetch('http://localhost:3000/students/' + studentId);
-      const result = await findStudent.json();
-      console.log(result);
-      res.json(result)
-    }
-    catch (error) {
-      console.log("Error message: " + error);
-    }
-  } else {
-    res.send('User does not exist.');
-  }
-})
+router.get(`/api/students/:id`, getStudentByIdController);
 
 //delete a student
 //works
-router.delete(`/api/students/:id`, (req: Request, res: Response) => {
-  const studentId = req.params.id;
-  console.log(studentId);
-
-  try {
-    const deleteStudent = fetch('http://localhost:3000/students/' + studentId, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-    res.status(204).end();
-  } catch (error) {
-    console.log('Error message: ' + error);
-  }
-})
+router.delete(`/api/students/:id`, deleteStudentByIdController);
 
 //update a student
 //works
-router.patch(`/api/students/:id`, async (req: Request, res: Response) => {
-  const getStudentId = req.params.id;
-  const patchStudent = req.body;
-
-  try {
-    const data = await fetch('http://localhost:3000/students/' + getStudentId, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "studentFirstName": patchStudent.studentFirstName,
-        "studentLastName": patchStudent.studentLastName,
-        "studentEmail": patchStudent.studentEmail
-      })
-    });
-    const response = await data.json();
-    console.log(response);
-    res.status(201).end();
-
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
-})
+router.patch(`/api/students/:id`, updateStudentByIdController);
 
 export default router;
