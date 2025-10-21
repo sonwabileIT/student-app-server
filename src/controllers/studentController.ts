6eeccc8d9c16fbedee50103eb5eb39f0eb454393
Sonwabile <123471279+sonwabileIT@users.noreply.github.com>
2025-10-21 21:50:33 +0200

    code: clean up comments

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -16,34 +16,12 @@ export const postNewStudentController = (req: Request, res: Response) => {
   const studentLastName: string = req.body.studentLastName;
   const studentEmail: string = req.body.studentEmail;
 
-
   const student = {
     id: id,
     studentFirstName: studentFirstName,
     studentLastName: studentLastName,
     studentEmail: studentEmail
   }
-  /*
-    try {
-  
-      const postStudent = fetch('http://localhost:3000/students', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          "id": newStudent.id,
-          "studentFirstName": newStudent.studentFirstName,
-          "studentLastName": newStudent.studentLastName,
-          "studentEmail": newStudent.studentEmail
-        }),
-      });
-      res.status(201).end();
-    }
-    catch (error) {
-      console.log("Error message: " + error);
-    }
-    */
 
   const newStudent = postNewStudent(student);
   res.status(201).send(newStudent);
@@ -55,22 +33,6 @@ export const getStudentByIdController = async (req: Request, res: Response) => {
   const studentId = req.params.id;
   const getStudent = await getStudentById(studentId);
   res.send(getStudent);
-  /*
-  if (studentId !== null) {
-
-    try {
-      const findStudent = await fetch('http://localhost:3000/students/' + studentId);
-      const result = await findStudent.json();
-      console.log(result);
-      res.json(result)
-    }
-    catch (error) {
-      console.log("Error message: " + error);
-    }
-  } else {
-    res.send('User does not exist.');
-  }
-*/
 }
 
 //works
@@ -79,19 +41,6 @@ export const deleteStudentByIdController = (req: Request, res: Response) => {
   const getStudent = deleteStudentById(studentId);
   res.status(204).end();
   //console.log(studentId);
-  /*
-    try {
-      const deleteStudent = fetch('http://localhost:3000/students/' + studentId, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-      res.status(204).end();
-    } catch (error) {
-      console.log('Error message: ' + error);
-    }
-  */
 
 }
 
@@ -101,26 +50,4 @@ export const updateStudentByIdController = async (req: Request, res: Response) =
   const patchStudent = req.body;
   const update = await updateStudentById(studentId, patchStudent);
   res.status(201).end();
-  /*
-    try {
-      const data = await fetch('http://localhost:3000/students/' + studentId, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          "studentFirstName": patchStudent.studentFirstName,
-          "studentLastName": patchStudent.studentLastName,
-          "studentEmail": patchStudent.studentEmail
-        })
-      });
-      const response = await data.json();
-      console.log(response);
-      res.status(201).end();
-  
-    }
-    catch (error) {
-      console.log("Error message: " + error);
-    }
-  */
 }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,17 +2,6 @@ import express, { Request, Response } from 'express';
 import { getAllUsersService, postUserService, getUserByIdService, updateUserByIdService, deleteUserByIdService } from '../services/userService';
 
 export const getAllUsersController = async (req: Request, res: Response) => {
-  /*
-  try {
-    const data = await fetch('http://localhost:3000/users');
-    const response = await data.json();
-    res.send(response);
-    console.log(response);
-  }
-  catch (error) {
-    console.log('Error Message: ' + error);
-  }
-*/
   const users = await getAllUsersService();
   console.log("From Controller: ", users);
   res.send(users);
@@ -23,44 +12,12 @@ export const postUserController = (req: Request, res: Response) => {
   const newUser = req.body;
   const userService = postUserService(newUser);
   res.status(201).end();
-  /*
-  try {
-    const addUser = fetch('http://localhost:3000/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "id": newUser.id,
-        "firstName": newUser.firstName,
-        "lastName": newUser.lastName,
-        "email": newUser.email,
-        "role": newUser.role
-      })
-    })
-    res.status(201).end();
-  }
-  catch (error) {
-    console.log('Error message: ' + error);
-  }
-*/
 };
 
 export const getUserByIdController = async (req: Request, res: Response) => {
   const getUserId = req.params.id;
   const user = await getUserByIdService(getUserId);
   res.send(user).end();
-  /*
-  try {
-    const getUser = await fetch('http://localhost:3000/users/' + getUserId);
-    const response = await getUser.json();
-    res.send(response);
-    console.log(response);
-  }
-  catch (error) {
-    console.log('Error message: ' + error);
-  }
-*/
 };
 
 export const updateUserByIdController = async (req: Request, res: Response) => {
@@ -69,29 +26,6 @@ export const updateUserByIdController = async (req: Request, res: Response) => {
   const userService = await updateUserByIdService(getUserId, patchUser);
   console.log("From Controller: ", userService)
   res.send(userService).end();
-  /*
-  try {
-    const updateUser = await fetch('http://localhost:3000/users/' + getUserId, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "firstName": patchUser.firstName,
-        "lastName": patchUser.lastName,
-        "email": patchUser.email,
-        "role": patchUser.role
-      })
-    });
-    const response = await updateUser.json();
-    res.status(200).end();
-    console.log(updateUser);
-
-  }
-  catch (error) {
-    console.log("Error message: " + error)
-  }
-*/
 };
 
 //works
@@ -99,19 +33,4 @@ export const deleteUserByIdController = (req: Request, res: Response) => {
   const getUserId = req.params.id;
   const deleteUser = deleteUserByIdService(getUserId);
   res.status(204).end();
-  /*try {
-    const deleteUser = fetch('http://localhost:3000/users/' + getUserId, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    res.status(204).end();
-
-  }
-  catch (error) {
-    console.log('Error message: ' + error)
-    res.end()
-  }
-*/
 };
