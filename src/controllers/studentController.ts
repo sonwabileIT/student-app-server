2d95c61cb5afb78989f4d96dda55d8a164134d3f
Sonwabile <123471279+sonwabileIT@users.noreply.github.com>
2025-10-21 18:34:49 +0200

    feat: add studentService and updated studentController

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,22 +1,15 @@
 import express, { Request, Response } from 'express';
+import { getAllStudents, getStudentById, postNewStudent, updateStudentById, deleteStudentById } from '../services/studentService';
 
+//Works
 export const getAllStudentsController = async (req: Request, res: Response) => {
-  try {
-    const students = await fetch('http://localhost:3000/students');
-    const response = await students.json();
-    if (response.length !== 0) {
-      console.log(response);
-      res.json(response);
-    } else {
-      res.send('There are no students');
-    }
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
+  const students = await getAllStudents();
+  console.log("From Controller: ", students);
+  res.send(students);
 
 };
 
+//works
 export const postNewStudentController = (req: Request, res: Response) => {
   const id: string = req.body.id;
   const studentFirstName: string = req.body.studentFirstName;
@@ -24,36 +17,45 @@ export const postNewStudentController = (req: Request, res: Response) => {
   const studentEmail: string = req.body.studentEmail;
 
 
-  const newStudent = {
+  const student = {
     id: id,
     studentFirstName: studentFirstName,
     studentLastName: studentLastName,
     studentEmail: studentEmail
   }
+  /*
+    try {
+  
+      const postStudent = fetch('http://localhost:3000/students', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "id": newStudent.id,
+          "studentFirstName": newStudent.studentFirstName,
+          "studentLastName": newStudent.studentLastName,
+          "studentEmail": newStudent.studentEmail
+        }),
+      });
+      res.status(201).end();
+    }
+    catch (error) {
+      console.log("Error message: " + error);
+    }
+    */
 
-  try {
+  const newStudent = postNewStudent(student);
+  res.status(201).send(newStudent);
 
-    const postStudent = fetch('http://localhost:3000/students', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "id": newStudent.id,
-        "studentFirstName": newStudent.studentFirstName,
-        "studentLastName": newStudent.studentLastName,
-        "studentEmail": newStudent.studentEmail
-      }),
-    });
-    res.status(201).end();
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
 };
 
+//works
 export const getStudentByIdController = async (req: Request, res: Response) => {
   const studentId = req.params.id;
+  const getStudent = await getStudentById(studentId);
+  res.send(getStudent);
+  /*
   if (studentId !== null) {
 
     try {
@@ -68,50 +70,57 @@ export const getStudentByIdController = async (req: Request, res: Response) => {
   } else {
     res.send('User does not exist.');
   }
-
+*/
 }
 
+//works
 export const deleteStudentByIdController = (req: Request, res: Response) => {
   const studentId = req.params.id;
-  console.log(studentId);
-
-  try {
-    const deleteStudent = fetch('http://localhost:3000/students/' + studentId, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-    res.status(204).end();
-  } catch (error) {
-    console.log('Error message: ' + error);
-  }
+  const getStudent = deleteStudentById(studentId);
+  res.status(204).end();
+  //console.log(studentId);
+  /*
+    try {
+      const deleteStudent = fetch('http://localhost:3000/students/' + studentId, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      res.status(204).end();
+    } catch (error) {
+      console.log('Error message: ' + error);
+    }
+  */
 
 }
 
+//works
 export const updateStudentByIdController = async (req: Request, res: Response) => {
-  const getStudentId = req.params.id;
+  const studentId = req.params.id;
   const patchStudent = req.body;
-
-  try {
-    const data = await fetch('http://localhost:3000/students/' + getStudentId, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "studentFirstName": patchStudent.studentFirstName,
-        "studentLastName": patchStudent.studentLastName,
-        "studentEmail": patchStudent.studentEmail
-      })
-    });
-    const response = await data.json();
-    console.log(response);
-    res.status(201).end();
-
-  }
-  catch (error) {
-    console.log("Error message: " + error);
-  }
-
+  const update = await updateStudentById(studentId, patchStudent);
+  res.status(201).end();
+  /*
+    try {
+      const data = await fetch('http://localhost:3000/students/' + studentId, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "studentFirstName": patchStudent.studentFirstName,
+          "studentLastName": patchStudent.studentLastName,
+          "studentEmail": patchStudent.studentEmail
+        })
+      });
+      const response = await data.json();
+      console.log(response);
+      res.status(201).end();
+  
+    }
+    catch (error) {
+      console.log("Error message: " + error);
+    }
+  */
 }
---
+++ src/services/studentService.ts
@@ -0,0 +1,109 @@
+
+type Student = {
+  id: string,
+  studentFirstName: string,
+  studentLastName: string,
+  studentEmail: string
+}
+
+//works
+export async function getAllStudents() {
+  try {
+    const students = await fetch('http://localhost:3000/students');
+    const response = await students.json();
+    if (response.length !== 0) {
+      console.log("From Service: ", response);
+    } else {
+      console.log('There are no students');
+    }
+    return response;
+  }
+  catch (error) {
+    console.log("Error message: " + error);
+  }
+
+};
+
+//works
+export function postNewStudent(student: Student) {
+  try {
+
+    const postStudent = fetch('http://localhost:3000/students', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "id": student.id,
+        "studentFirstName": student.studentFirstName,
+        "studentLastName": student.studentLastName,
+        "studentEmail": student.studentEmail
+      }),
+    });
+    //res.status(201).end();
+  }
+  catch (error) {
+    console.log("Error message: " + error);
+  }
+
+}
+
+//works
+export async function getStudentById(id: string) {
+  if (id !== null) {
+
+    try {
+      const findStudent = await fetch('http://localhost:3000/students/' + id);
+      const result = await findStudent.json();
+      console.log(result);
+      return result;
+    }
+    catch (error) {
+      console.log("Error message: " + error);
+    }
+  } else {
+    console.log('User does not exist.');
+  }
+
+}
+
+//works
+export async function updateStudentById(id: string, student: Student) {
+  try {
+    const data = await fetch('http://localhost:3000/students/' + id, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "studentFirstName": student.studentFirstName,
+        "studentLastName": student.studentLastName,
+        "studentEmail": student.studentEmail
+      })
+    });
+    const response = await data.json();
+    console.log(response);
+    //res.status(201).end();
+
+  }
+  catch (error) {
+    console.log("Error message: " + error);
+  }
+
+}
+
+//works
+export function deleteStudentById(id: string) {
+  try {
+    const deleteStudent = fetch('http://localhost:3000/students/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    //res.status(204).end();
+  } catch (error) {
+    console.log('Error message: ' + error);
+  }
+
+}
