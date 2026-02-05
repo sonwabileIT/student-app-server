f8e626d93d4eb4f1fb0af4be0c185626b7f794b0
Sonwabile <123471279+sonwabileIT@users.noreply.github.com>
2026-02-05 18:07:37 +0200

    feat: add backend login skeleton

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 //import session from 'express-session';
 import { postUserController } from '../controllers/userController';
 import { postNewStudentController } from '../controllers/studentController';
+import { postNewStudent } from '../services/studentService';
 
 const app = express();
 app.use(express.json());
@@ -14,16 +15,38 @@ router.get('/api/auth', (req: Request, res: Response) => {
   console.log("req.sessionID: " + req.sessionID);
 })
 //signup
-router.post('users/register', postUserController);
+//router.post('users/register', postUserController);
 
 //router.post('/api/auth/students/register', postNewStudentController);
 router.post('/api/auth/students/register', (req: Request, res: Response) => {
+
+  //post student
+  const id: string = req.body.id;
+  const studentFirstName: string = req.body.studentFirstName;
+  const studentLastName: string = req.body.studentLastName;
+  const studentEmail: string = req.body.studentEmail;
+  const password: string = req.body.password;
+
+  const student = {
+    id: id,
+    studentFirstName: studentFirstName,
+    studentLastName: studentLastName,
+    studentEmail: studentEmail,
+    password: password
+  }
+
+  const newStudent = postNewStudent(student);
+  res.status(201).send(newStudent);
+
   //after post method
   //
-  //set session
-  //req.session.email = req.body.studentEmail;
-  //req.session.password = req.body.password;
-
+  //View session
+  req.session.studentID = req.body.id;
+  req.session.studentEmail = req.body.studentEmail;
+  //console.log("Session: ", req.session);
+  //console.log("SessionID: ", req.sessionID);
+  //console.log("Session: ", req.session);
+  //console.log("SessionID: ", req.sessionID);
 })
 
 //login
